0269ca69da763e0bf14eab52f317b9c0136357e4
Matheus Riguette <matheus.f.riguette@gmail.com>
2022-08-24 13:41:30 -0300

    a

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const getPages = (pageNumber) => {
-  const pages = Array.from({ length: pageNumber }, (_, i) => i + 1);
+const getPages = (pageNumber, startPage) => {
   let firstPrint = [];
   let secondPrint = [];
   let currentToAdd = "firstPrint";
   let count = 0;
 
-  pages.forEach((i) => {
+  for (let i = startPage; i <= pageNumber; i++) {
     count++;
 
     if (currentToAdd === "firstPrint") {
@@ -20,15 +19,16 @@ const getPages = (pageNumber) => {
         : (currentToAdd = "firstPrint");
       count = 0;
     }
-  });
+  }
 
   return [firstPrint, secondPrint];
 };
 
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
-  const pageNumber = document.querySelector("input").value;
-  const [firstPrint, secondPrint] = getPages(pageNumber);
+  const pageNumber = document.querySelector("#pageNumber").value;
+  const startPage = document.querySelector("#startPage").value;
+  const [firstPrint, secondPrint] = getPages(pageNumber, startPage);
 
   const firstPrintElement = document.querySelector("#firstPrint");
   const secondPrintElement = document.querySelector("#secondPrint");
